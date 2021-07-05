7d2028f403448f2d872e7090057be18c0bea0249
Charles Feval <c@feval.ca>
2021-07-04 22:54:22 -0700

    Fix todo parsing

--- test/FileParser.test.ts
+++ test/FileParser.test.ts
@@ -1,5 +1,4 @@
 import * as should from "should"
-import { fakeContext } from "./FakeContext"
 import { makeFakeDeps } from "./FakeDeps"
 import * as td from "testdouble"
 import { FileParser } from "../src/domain/FileParser"
@@ -35,26 +34,68 @@ Some text
     const todos = fileParser.parseFile(content, file)
 
     // then
-    
-    const objectEql = (expected: {[key: string]: any}, actual: {[key: string]: any}): boolean => {
-      return !Object.keys(expected).find((key) => 
-        {
-          if (typeof expected[key] === "object") {
-            return actual[key] 
-              && typeof actual[key] === "object" 
-              && objectEql(expected[key], actual[key])
-          }
+
+    const objectEql = (expected: { [key: string]: any }, actual: { [key: string]: any }): boolean => {
+      const keys = Object.keys(expected)
+      const actualDoesntMatchForKey = (key: string) => {
+        if (typeof expected[key] === "object") {
+          return actual[key] === undefined
+            || typeof actual[key] !== "object"
+            || !objectEql(expected[key], actual[key])
+        } else {
           return expected[key] !== actual[key]
-        })
+        }
+      }
+      return keys.length === 0 || !keys.find(actualDoesntMatchForKey)
     }
 
     const findObj = (array: object[], expected: object) => {
       return array.find(obj => objectEql(expected, obj))
     }
 
-    it("should load normal todo", () => should(findObj(todos, { status: TodoStatus.Todo, text: "A todo todo", file, project: projectName, line: aTodoTodoLn })).not.be.undefined())
-    it("should load completed todo", () => should(findObj(todos, { status: TodoStatus.Complete, text: "a completed todo", file: file, project: projectName, attributes: { assignee: "Pete", booleanAttribute: true }, line: aTodoWithProjectLn })).not.be.undefined())
-    it("should load delegated todo", () => should(findObj(todos, { status: TodoStatus.Delegated, text: "a delegated todo", file, project: projectName, attributes: { assignee: "Leah", anotherBooleanAttr: true }, line: aDelegatedTodoLn })).not.be.undefined())
-    it("should load in progress todo", () => should(findObj(todos, { status: TodoStatus.InProgress, text: "An in progress todo", file, project: projectName, attributes: {}, line: anInProgressTodo })).not.be.undefined())
+    const containsTodo = (expected: Partial<TodoItem>) => {
+      const found = findObj(todos, expected)
+      if (!found) {
+        throw (Error(`Expected to find ${JSON.stringify(expected, null, 2)} in ${JSON.stringify(todos, null, 2)}`))
+      }
+    }
+
+    it("should load normal todo", () => containsTodo({
+      status: TodoStatus.Todo,
+      text: "A todo todo",
+      file,
+      project: projectName,
+      line: aTodoTodoLn
+    }))
+    it("should load a todo with a project", () => containsTodo(
+      {
+        text: "a todo with",
+        project: "this project",
+        attributes: { "project": "this project" },
+      }))
+    it("should load completed todo", () => containsTodo(
+      {
+        status: TodoStatus.Complete,
+        text: "a completed todo",
+        file: file,
+        project: projectName,
+        attributes: { assignee: "Pete", booleanAttribute: true },
+        line: aCompletedTodoLn
+      }))
+    it("should load delegated todo", () => containsTodo({
+      status: TodoStatus.Delegated,
+      text: "a delegated todo", file,
+      project: projectName,
+      attributes: { assignee: "Leah", anotherBooleanAttr: true },
+      line: aDelegatedTodoLn
+    }))
+    it("should load in progress todo", () => containsTodo({
+      status: TodoStatus.InProgress,
+      text: "An in progress todo",
+      file,
+      project: projectName,
+      attributes: {},
+      line: anInProgressTodo
+    }))
   })
 })
